56ec6352d1eaa1101769ea75a8172ee61a0b9288
Shoko Hashimoto <shoko@shokos-mbp-2.home>
2025-10-05 14:08:06 -0400

    chore(project): Add Xcode project

---
+++ PodcastsApp/PodcastsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PodcastsApp
+//
+//  Created by Shoko Hashimoto on 2025-10-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PodcastsApp/PodcastsApp/PodcastsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastsAppApp.swift
+//  PodcastsApp
+//
+//  Created by Shoko Hashimoto on 2025-10-05.
+//
+
+import SwiftUI
+
+@main
+struct PodcastsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
